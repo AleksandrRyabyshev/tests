71eeecac265baa3df50a98e29d3f85e36a0c06bf
Ryabyshev Aleksandr <ssori3838@gmail.com>
2019-04-11 00:22:48 +0300

    add Redux

--- app/Constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App;
-
-class Constants
-{
-    const DB_STRING = 'string|min:1|max:255';
-}
--- resources/js/store/types.js
+++
@@ -1,4 +0,0 @@
-export const LOGIN_IS_LOADING = 'LOGIN_IS_LOADING';
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-export const LOGIN_ERROR = 'LOGIN_ERROR';
-export const IS_LOGGED = 'IS_LOGGED';
